019fdfeac9f5867c3ac069006f731e86e8e1fa60
Vadym Vyshnevskyi <wallian169@gmail.com>
2026-03-01 11:09:55 +0200

    fix: change name

--- task.sql
+++ task.sql
@@ -1,6 +1,6 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB; 
 
-CREATE index idx_email ON Customers (Email);
+CREATE index Email ON Customers (Email);
 
-CREATE index idx_name on Products (Name);
+CREATE index Name on Products (Name);
